cd87add42116ed40c14c4593d589a74ebd116bee
paosua86 <paolasuarezb1986@gmail.com>
2023-01-09 15:45:00 -0500

    task4

---
+++ module1_task4/setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+apt-get update && apt-get install -y hugo wget
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
+tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
+mv hugo /usr/local/bin/
+rm hugo_extended_0.109.0_Linux-64bit.tar.gz
+# generate a hugo website
+make build
