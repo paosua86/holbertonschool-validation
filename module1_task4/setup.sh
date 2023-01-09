c3d1a976a57a2860770b05a6032969782951bf25
paosua86 <paolasuarezb1986@gmail.com>
2023-01-09 15:48:56 -0500

    setup

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,9 +1,6 @@
-#!/usr/bin/env bash
-
-apt-get update && apt-get install -y hugo wget
-wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
-tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
-mv hugo /usr/local/bin/
-rm hugo_extended_0.109.0_Linux-64bit.tar.gz
-# generate a hugo website
+#!/bin/bash
+apt-get update && apt-get install -y hugo make
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
+apt install ./hugo.deb
+rm hugo.deb
 make build
