b3bdff05372e4446dc8f2b2f8c2d3e53d2dec6f0
paosua86 <paolasuarezb1986@gmail.com>
2023-01-11 08:52:11 -0500

    tests

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-apt-get update && apt-get install -y make wget
-wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
-dpkg -i hugo_extended_0.109.0_linux-amd64.deb
-rm -f hugo_extended_0.109.0_linux-amd64.deb
+#!/bin/bash
+apt-get update && apt-get install -y make build-essential wget && wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb && dpkg -i hugo_extended_0.109.0_linux-amd64.deb
+make build
