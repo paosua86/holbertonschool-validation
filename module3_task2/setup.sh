f3d47e95c45e08c04763f708387199d1c1937292
paosua86 <paolasuarezb1986@gmail.com>
2023-01-11 09:01:16 -0500

    task 2

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-apt-get update && apt-get install -y make build-essential wget && wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb && dpkg -i hugo_extended_0.109.0_linux-amd64.deb
+docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
+sudo apt-get update && sudo apt-get install -y hugo make
+sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o last.deb
+sudo apt install ./last.deb
+sudo rm last.deb
 make build
