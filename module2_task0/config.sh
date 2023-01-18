f13e308a68bd6e8ac3eb1b7d8c1666651161ba58
paosua86 <paolasuarezb1986@gmail.com>
2023-01-18 11:44:04 -0500

    go.sum

--- module2_task0/config.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.5.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-go get -u github.com/gorilla/mux
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.50.1
